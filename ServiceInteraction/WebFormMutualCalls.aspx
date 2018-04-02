d3122f5a5d60d6c02102cafc00548bd23a0cc29d
NRGb3nder <energybender120@gmail.com>
2018-04-02 22:22:33 +0300

    Add mutual calls web form

---
+++ ServiceInteraction/WebFormMutualCalls.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebFormMutualCalls.aspx.cs" Inherits="ServiceInteraction.WebFormMutualCalls" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Service Interaction</title>
+</head>
+<body>
+    <form id="form1" runat="server" style="font-family: Arial;">
+        <p>
+            <asp:Label ID="LabelCallCount" runat="server" Text="Number of calls: "></asp:Label>
+            <asp:TextBox ID="TextBoxCallCount" runat="server"></asp:TextBox>
+            <asp:Button ID="ButtonCall" runat="server" Text="Call!" Width="52px" Height="21px" OnClick="ButtonCall_Click" />
+        </p>
+        <p>
+            <asp:TextBox ID="TextBoxResult" ReadOnly="true" TextMode="MultiLine" runat="server" Height="130px" Width="450px"></asp:TextBox>
+        </p>
+    </form>
+</body>
+</html>
---
+++ ServiceInteraction/WebFormMutualCalls.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ServiceInteraction
+{
+    public partial class WebFormMutualCalls : System.Web.UI.Page
+    {
+        private KenobiReference.KenobiServiceSoapClient jediClient;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.jediClient = new KenobiReference.KenobiServiceSoapClient();
+        }
+
+        protected void ButtonCall_Click(object sender, EventArgs e)
+        {
+            if (TextBoxCallCount.Text != "")
+            {
+                TextBoxResult.Text = "";
+                uint callCount;
+                TextBoxResult.Text = UInt32.TryParse(TextBoxCallCount.Text, out callCount) ?
+                    jediClient.SayHi(callCount, "") : "Invalid call count";
+            }
+        }
+    }
+}
