3f15ceaec94415e2a629bdd122f2699346f03e88
NRGb3nder <energybender120@gmail.com>
2018-04-02 21:40:54 +0300

    Add ICommunicableJedi interface

---
+++ ServiceInteraction/ICommunicableJedi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceInteraction
+{
+    interface ICommunicableJedi
+    {
+        string SayHi(uint greetingsCounter, string log);
+    }
+}
